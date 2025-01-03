6314273041ff6f7085b3f9f31a477eef6842c941
Muhammad Muzakkir Bin Dzulkarnain <126316479+muzakkir280595@users.noreply.github.com>
2025-01-03 21:58:18 +0800

    Learning Switch function
    Switch function looks like if else function it just easy to read and have better performance.

---
+++ Java/Programming with Eclipse/Basics Java/05 Booleans/Booleans/src/booleans.java
@@ -0,0 +1,13 @@
+
+public class booleans {
+
+	public static void main(String[] args) {
+		int x = 9;
+		int y = 8;
+		boolean result = (x > y);
+				
+		System.out.println("Is "+x+">"+y+" Answer:"+result);
+
+	}
+
+}
---
+++ Java/Programming with Eclipse/Basics Java/06 Switch Statements/switch/src/switchs.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class switchs {
+	
+	//Switch have little bit same function with if else statement, just it is more easy to read and better for performance.
+	//break is needed for switch function to avoid it to go to the next switch or test cases.
+	//Case expression must be constant expression. Constant expression is something that we not declare like int y=1 then we change case from case 1 to case y.It MUST case 1.
+
+	public static void main(String[] args) {
+		int year;
+		Scanner keyboard = new Scanner (System.in);
+		System.out.println("Enter your experince working year:");
+		year = keyboard.nextInt();
+		
+		switch (year) {
+		case 1:
+			System.out.println("Freshman");
+			break;
+		case 2:
+			System.out.println("Sophomore");
+			break;
+		case 3:
+			System.out.println("Junior");
+			break;
+		case 4:
+			System.out.println("Senior");
+			break;  
+		case 5:
+			System.out.println("Super Senior"); //The end of this case break does not need to put also can but I'm usually put just to be safe.
+		}
+		
+		System.out.println("Are you CRAZY!!!!You Are More Than SUPER SENIOR!!!!");
+
+	}
+
+}
